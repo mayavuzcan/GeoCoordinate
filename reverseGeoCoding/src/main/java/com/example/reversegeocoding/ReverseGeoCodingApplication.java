c765aeed060573a4ac6d12f614a50784c5251eeb
EXT02D47094 <QGJqj487>
2022-06-15 14:55:18 +0300

    Created Project

---
+++ reverseGeoCoding/src/main/java/com/example/reversegeocoding/ReverseGeoCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.reversegeocoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseGeoCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReverseGeoCodingApplication.class, args);
+    }
+
+}
---
+++ reverseGeoCoding/src/test/java/com/example/reversegeocoding/ReverseGeoCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reversegeocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseGeoCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
